59365dd5df9c501256c049a8c29349e40408e33e
Mario Salles <mariofsalles@gmail.com>
2021-04-01 15:24:09 +0000

    Trabalhando com pacotes exteriores: locais ou remotos

--- 01-Pacotes/go.mod
+++ 01-Pacotes/go.mod
@@ -1,3 +1,5 @@
 module module01
 
 go 1.16
+
+require github.com/badoux/checkmail v1.2.1
--- 01-Pacotes/main.go
+++ 01-Pacotes/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"module01/auxiliar"
+	"github.com/badoux/checkmail"
 )
 
 func main(){
 	fmt.Println("Escrecendo num arquivo main")
 	auxiliar.Escrever()
+	erro := checkmail.ValidateFormat("mariosalles@gmail.com")
+	fmt.Println(erro)
 }
