dfed84e162a0040eddb789aa62c4f3e1fc9b5046
Mario Salles <mariofsalles@gmail.com>
2021-04-09 22:51:39 +0000

    Funcoes 2

--- 04-Funcoes/funcoes.go
+++ 04-Funcoes/funcoes.go
@@ -7,11 +7,20 @@ var f = func() {
 }
 
 var soma = func(a int8, b int8) {
-	fmt.Println(a + b) 
+	fmt.Println(a + b)
+}
+
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := (n1 + n2)
+	subtracao := n1 - n2
+	return soma, subtracao
 }
 
 
-func main(){
-	soma(2,5)
+func main() {
+	soma(2, 5)
 	f()
+
+	_ , resultadoSubtracao := calculosMatematicos(5, 3)
+	fmt.Println(resultadoSubtracao)
 }
