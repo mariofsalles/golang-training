360eca43f0a873b07bd6e41db51aed98bdbf839d
Mario Salles <mariofsalles@gmail.com>
2021-05-11 16:50:24 +0000

    Multiplexador

---
+++ 18-Concorrencia/18.6-Padroes de Concorrencia/18.6.3-Multiplexador/multiplexador.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main()  {
+	canal := multiplexar(escrever("Olá mundo!"),escrever("Programando em Go!"))
+	for i:=0; i <10; i++{
+		fmt.Println(<-canal)
+	}
+}
+
+func multiplexar(canalEntrada1, canalEntrada2 <-chan string)  <-chan string {
+	canalDeSaida := make(chan string)
+
+	go func(){
+		for{
+			select{
+			case mensagem := <-canalEntrada1:
+				canalDeSaida <-mensagem
+			case mensagem := <-canalEntrada2:
+				canalDeSaida <-mensagem
+			}
+		}
+	}()
+	return canalDeSaida
+}
+
+func escrever(texto string) <-chan string {
+	canal := make(chan string)
+	go func() {
+		for{
+			canal <- fmt.Sprintf("Valor recebido: %s", texto)
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(1000)))
+		}
+	}()
+	return canal
+}
